6c94d45b95442803f1396034810acb181778988a
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-10-31 17:50:26 -0300

    product/:product route

--- routes/product.js
+++ routes/product.js
@@ -8,4 +8,9 @@ router.get('/', async (req, res) => {
   res.status(200).send(results[0])
 })
 
+router.get('/:product', async (req, res) => {
+  const results = await db.promise().query(`SELECT * FROM product WHERE name LIKE '%${req.params.product}%';`)
+  res.status(200).send(results[0])
+})
+
 module.exports = router
