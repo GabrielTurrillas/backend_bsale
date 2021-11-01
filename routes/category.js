22f2a4a4c8fd79ee021bc2c1201cb03a99a9b1c0
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-11-01 14:26:41 -0300

    error handling

--- routes/category.js
+++ routes/category.js
@@ -4,8 +4,12 @@ const db = require('../database');
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const results = await db.promise().query(`SELECT * FROM category;`)
-  res.status(200).send(results[0])
+  try {
+    const results = await db.promise().query(`SELECT * FROM category;`)
+    res.status(200).send(results[0])
+  } catch (e) {
+    res.send(e)
+  }
 });
 
 module.exports = router
--- routes/product.js
+++ routes/product.js
@@ -4,18 +4,30 @@ const db = require('../database');
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const results = await db.promise().query(`SELECT * FROM product;`)
-  res.status(200).send(results[0])
+  try {
+    const results = await db.promise().query(`SELECT * FROM product;`)
+    res.status(200).send(results[0])
+  } catch (e) {
+    res.send(e)
+  }
 })
 
 router.get('/:product', async (req, res) => {
-  const results = await db.promise().query(`SELECT * FROM product WHERE name LIKE '%${req.params.product}%';`)
-  res.status(200).send(results[0])
+  try {
+    const results = await db.promise().query(`SELECT * FROM product WHERE name LIKE '%${req.params.product}%';`)
+    res.status(200).send(results[0])
+  } catch (e) {
+    res.send(e)
+  }
 })
 
 router.get('/category/:id', async (req, res) => {
-  const results = await db.promise().query(`SELECT * FROM product WHERE category=${req.params.id};`)
-  res.status(200).send(results[0])
+  try {
+    const results = await db.promise().query(`SELECT * FROM product WHERE category=${req.params.id};`)
+    res.status(200).send(results[0])
+  } catch (e) {
+    res.send(e)
+  }
 })
 
 module.exports = router
